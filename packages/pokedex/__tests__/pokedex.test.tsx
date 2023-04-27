0b2c49c8421ebe91260f21fd0c6af8f307890359
Anurag Nagar <anuragnagarkota@gmail.com>
2023-04-27 20:49:00 +0530

    minor fix

--- packages/pokedex/__tests__/pokedex.test.tsx
+++ packages/pokedex/__tests__/pokedex.test.tsx
@@ -1,17 +1,17 @@
 import "@testing-library/jest-dom";
-import { queryByAttribute, render, screen } from "@testing-library/react";
+import {  render, screen } from "@testing-library/react";
 
 import { PokemonCard } from "components";
 
 const obj = { name: "pokemon", height: 10, weight: 20 };
 it("should render", () => {
-  const { getByPlaceholderText } = render(<PokemonCard {...obj} />);
+   render(<PokemonCard {...obj} />);
   const table = screen.getByRole("table");
   expect(table).toBeInTheDocument();
 });
 
 it("props rendered correctly", () => {
-  const { getByPlaceholderText } = render(<PokemonCard {...obj} />);
+  render(<PokemonCard {...obj} />);
   screen.getByRole("table");
   expect(screen.getByText(/pokemon/i)).toBeInTheDocument();
 });
